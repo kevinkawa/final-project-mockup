cab51b57db3b6bdc440bdf62321b87d167a499bb
kevinkawa <kkawa@hawaii.edu>
2017-03-20 05:20:48 -1000

    1st attempt not finished

---
+++ app/imports/ui/layouts/index.js
@@ -0,0 +1,10 @@
+import './app-body.html';
+import './app-body.js';
+import './app-not-found.html';
+import './footer.html';
+import './footer.js';
+import './header.html';
+import './header.js';
+import './if-logged-in.html';
+import './if-logged-in.js';
+import './loading.html';
